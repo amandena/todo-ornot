5d9b449686203de238a4b9dcc3271ae64625ee84
amandena <manda.m.johnson@gmail.com>
2020-10-21 22:09:57 -0400

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-let destination = document.querySelector("#container")
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  destination
   document.getElementById('root')
 )
